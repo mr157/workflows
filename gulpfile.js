10093730d84da2d3e177c97eefe42541c46104c1
John <John@Noels-iMac.local>
2017-12-05 21:33:59 +0000

    Added Browserfiy to include/require jquery

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp')
     , gutil = require('gulp-util')
     , coffee = require('gulp-coffee')
-    , concat = require('gulp-concat');
+    , concat = require('gulp-concat')
+    , browserify = require('gulp-browserify');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = ['components/scripts/rclick.js', 'components/scripts/pixgrid.js', 'components/scripts/tagline.js', 'components/scripts/template.js'
@@ -23,5 +24,6 @@ gulp.task('coffee', function () {
 gulp.task('js', function () {
     gulp.src(jsSources)
         .pipe(concat('script.js'))
+        .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
 });
