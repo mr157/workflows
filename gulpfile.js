561e6418c60ea9163198b0a4fba0b8cbd458c940
John <John@Noels-iMac.local>
2017-12-05 21:02:14 +0000

    Added gulp concat and concat script

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var coffee = require('gulp-coffee');
+var gulp = require('gulp')
+    , gutil = require('gulp-util')
+    , coffee = require('gulp-coffee')
+    , concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = ['components/scripts/rclick.js', 'components/scripts/pixgrid.js', 'components/scripts/tagline.js', 'components/scripts/template.js'
+];
+
 gulp.task('log', function () {
     gutil.log('Workflows are awesome');
 });
@@ -14,4 +18,10 @@ gulp.task('coffee', function () {
             })
             .on('error', gutil.log))
         .pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function () {
+    gulp.src(jsSources)
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('builds/development/js'))
 });
