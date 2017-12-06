4bf142d0f770684cedca5717fcc08ff5c3ca3935
John <John@Noels-iMac.local>
2017-12-06 14:47:03 +0000

    Added Sass Compiler

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
-var gulp = require('gulp')
-    , gutil = require('gulp-util')
-    , coffee = require('gulp-coffee')
-    , concat = require('gulp-concat')
-    , browserify = require('gulp-browserify');
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat'),
+    browserify = require('gulp-browserify'),
+    compass = require('gulp-compass');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
-var jsSources = ['components/scripts/rclick.js', 'components/scripts/pixgrid.js', 'components/scripts/tagline.js', 'components/scripts/template.js'
+var jsSources = ['components/scripts/rclick.js',
+                 'components/scripts/pixgrid.js',
+                 'components/scripts/tagline.js',
+                 'components/scripts/template.js'
 ];
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('log', function () {
     gutil.log('Workflows are awesome');
@@ -26,4 +31,19 @@ gulp.task('js', function () {
         .pipe(concat('script.js'))
         .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function () {
+    return gulp.src(sassSources)
+        .pipe(compass({
+            css: 'builds/development/css',
+            sass: 'components/sass',
+            image: 'builds/development/images',
+            comments: 'true',
+            style: 'expanded'
+        }))
+        .on('error', gutil.log)
+
+    .pipe(gulp.dest('builds/development/css'));
+
 });
